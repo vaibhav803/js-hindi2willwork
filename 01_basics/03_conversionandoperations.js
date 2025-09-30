8aafbf1c4c34fc86111b0d100651a122a36ef17f
vaibhav803 <grade2512@gmail.com>
2025-09-30 11:32:36 +0000

    did conditionals

---
+++ 01_basics/03_conversionandoperations.js
@@ -0,0 +1,77 @@
+let string = "33abc"
+
+
+let number = Number(string)
+
+
+// console.log(typeof number);
+// console.log(number);
+
+
+
+
+let num = "ferenzi";
+
+let bool = Boolean(num);
+
+
+// console.log(typeof bool);
+// console.log(bool);
+
+
+let val = undefined
+
+let nums = Number(val);
+console.log(typeof nums);
+console.log(nums);
+
+// nan -> type is number
+//null -> type is object
+
+
+// ****************** Operation ********************
+
+
+let value = 3
+let negValue = -value
+
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+
+// console.log(2*2);
+
+
+// console.log(2/2);
+
+
+// // console.log(22);
+
+
+// console.log(2**2);
+
+
+// console.log(2%2);
+
+
+// console.log("1" + 2 +3);
+// console.log(1 + 2 +"3");
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+let gameCOunter = 100
+gameCOunter++
+
+console.log(gameCOunter);
+
+
+
+
+
+
---
+++ 01_basics/04_conditionals.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 <1);
+// console.log( 2 ==1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+
+
+
+
+
+
+// === Strict checking checks if they are same datatype 
+
