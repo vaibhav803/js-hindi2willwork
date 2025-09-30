c58e240faa5d3817881e43040bb71d0ee016ada3
vaibhav803 <grade2512@gmail.com>
2025-09-30 10:36:36 +0000

    hello datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445;
+let accountEmail = "hit@321@gmail.com";
+var accountpassword = "12345";
+accountCity = "Jaipur";
+
+accountEmail = "hit@@gmail.com";
+accountpassword = "1345";
+accountCity = "djsd"
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountpassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+// alert(3+3);
+
+console.log(3 + 3);
+
+console.log("Hitu");
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof undefined); // type is undefined
+
+console.log(typeof null);   // type is object
+
