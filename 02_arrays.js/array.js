5cbb06303ca4437331dd403324f5cd72d59067c0
vaibhav803 <grade2512@gmail.com>
2025-09-30 16:08:07 +0000

    arrays beware splice

---
+++ 02_arrays.js/array.js
@@ -0,0 +1,51 @@
+// array
+
+const myArr = [0, 9,5,3,2]
+const myHeroes = ["taleb","ramanujan","mcgilchrist"]
+
+
+const myArr2 = new Array(1,2,3)
+// console.log(myArr[1]);
+
+
+// myArr.push(6)
+// console.log(myArr)
+// myArr.push(7)
+
+// console.log(myArr)
+
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.indexOf(3));
+// console.log(myArr));
+
+
+// console.log(typeof myArr.join())
+
+// console.log(myArr.includes(99));
+
+
+
+
+
+
+
+console.log("A ",myArr)
+
+console.log(myArr.slice(2,5))
+
+
+console.log("B ",myArr)
+
+console.log(myArr.splice(2,4))
+
+
+console.log("C ", myArr);
+
+
+
