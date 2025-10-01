4255cdb49380d9bf23e58290e8299c814d636665
vaibhav803 <grade2512@gmail.com>
2025-10-01 06:36:31 +0000

    functions and scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,75 @@
+let a = 100
+
+if (true) {
+    let a = 10;
+    var b = 20;
+    const c = 30;
+    // console.log(a)
+}
+
+b = 600
+
+// console.log(a);
+// console.log(a);
+// console.log(c);
+
+
+function One() {
+    const username = "hello github"
+
+    function Two(){
+        const website = "utube"
+        console.log(username);
+        
+    }
+    console.log("web");
+
+    Two()
+    
+}
+
+
+// One()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = "yotube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++ interesting  +++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+
+// also called expression 
+
+
+//it is a error so remember
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
+
